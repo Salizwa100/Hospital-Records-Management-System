010ec0164982a339b0c90122184dbfc0c921a1bf
Salizwa <sv.ntlabati@gmail.com>
2025-04-20 13:28:15 +0200

    Create test_user_factory.py

---
+++ tests/test_user_factory.py
@@ -0,0 +1,11 @@
+import unittest
+from factory.user_factory import UserFactory
+
+class TestUserFactory(unittest.TestCase):
+    def test_create_doctor(self):
+        doctor = UserFactory.create_user("doctor", doctorId="U100", name="Dr. Who", specialization="Cardiology", contactInfo="123")
+        self.assertEqual(doctor.name, "Dr. Who")
+        self.assertEqual(doctor.role, "Doctor")
+
+if __name__ == "__main__":
+    unittest.main()
